2b9e4c4226c52d00144d797daeaddda27e439ad7
aishaar <aishaar283@gmail.com>
2025-01-16 12:03:33 -0800

    Added Button Logic

--- app/src/main/java/com/example/numad25sp_fnuaisha/MainActivity.java
+++ app/src/main/java/com/example/numad25sp_fnuaisha/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.numad25sp_fnuaisha;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
+        Button aboutMeButton = findViewById(R.id.aboutMeButton);
+        aboutMeButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Name= Fnu Aisha \nEmail = aisha.f@northeastern.edu", Toast.LENGTH_LONG).show();
+        } );
+
+
+
+    }
 }
